a55d5389dd2cee462d719ec6d2903f1e3dfab83c
Zain Aftab <zainaftab44@gmail.com>
2020-05-06 11:19:33 +0500

    updating layout and display more data

--- app/src/main/java/com/sleepycoder/randomuserapi/MainActivity.java
+++ app/src/main/java/com/sleepycoder/randomuserapi/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,7 +24,8 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_name, tv_email, tv_gender;
+    TextView tv_name, tv_email, tv_gender, tv_birthdate, tv_age;
+    ImageView iv_user_picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         tv_name = findViewById(R.id.tv_name);
         tv_email = findViewById(R.id.tv_email);
         tv_gender = findViewById(R.id.tv_gender);
+        tv_birthdate = findViewById(R.id.tv_birthdate);
+        tv_age = findViewById(R.id.tv_age);
+        iv_user_picture = findViewById(R.id.iv_user_picture);
 
         findViewById(R.id.btn_generate).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,11 +72,22 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject user = results.getJSONObject(0);
                     JSONObject nameObj = user.getJSONObject("name");
                     String name = nameObj.getString("title") + ". " + nameObj.getString("first") + " " + nameObj.getString("last");
-                    String email = user.getString("email");
-                    String gender = user.getString("gender");
                     publishProgress(name, 0);
+
+                    String email = user.getString("email");
                     publishProgress(email, 1);
+
+                    String gender = user.getString("gender");
                     publishProgress(gender, 2);
+
+                    String image_url = user.getJSONObject("picture").getString("medium");
+                    publishProgress(image_url, 3);
+
+                    String birthdate = user.getJSONObject("dob").getString("date");
+                    publishProgress(birthdate, 4);
+
+                    String age = user.getJSONObject("dob").getString("age");
+                    publishProgress(age, 5);
 
                 } catch (JSONException ex) {
                     ex.printStackTrace();
@@ -89,14 +107,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, values[0].toString(), Toast.LENGTH_SHORT).show();
             switch (Integer.parseInt(values[1] + "")) {
                 case 0:
-                    tv_name.setText(values[0].toString());
+                    tv_name.setText("Name: " + values[0].toString());
                     break;
                 case 1:
-                    tv_email.setText(values[0].toString());
+                    tv_email.setText("Email: " + values[0].toString());
                     break;
                 case 2:
-                    tv_gender.setText(values[0].toString());
+                    tv_gender.setText("Gender: " + values[0].toString());
                     break;
+                case 3:
+                    Picasso.get().load(values[0].toString()).into(iv_user_picture);
+                case 4:
+                    tv_birthdate.setText("Date: " + values[0]);
+                case 5:
+                    tv_age.setText("Age: " + values[0]);
             }
         }
     }
